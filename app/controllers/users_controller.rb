ba5c958ba89432f8da4024d80dc4156276179413
ramirosc <ramiro@chicagobooth.edu>
2015-11-19 15:14:05 -0600

    hw
    user/alice
    name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,10 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def show
+  @user = User.find(params[:id])
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -57,6 +57,7 @@ Rails.application.routes.draw do
 
 
   get "/users", :controller => "users", :action => "index"
+  get "/users/:id", :controller => "users", :action => "show"
 
 
 end
